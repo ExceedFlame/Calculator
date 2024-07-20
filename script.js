6f2bb96d325e6c173256bbe36c5e05b9ca543736
ExceedFlame <43750908+ExceedFlame@users.noreply.github.com>
2024-07-20 12:56:28 +0530

    Add keyboard support

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-let input = document.getElementById("inputBox")
-let buttons = document.querySelectorAll("button")
-expression = ''
-let arr = Array.from(buttons)
+let input = document.getElementById("inputBox");
+let buttons = document.querySelectorAll("button");
+expression = '';
+let arr = Array.from(buttons);
+
 arr.forEach(button => {
     button.addEventListener('click', (e) => {
-        if (e.target.innerHTML === '=') {
-            expression = eval(expression)
-            input.value = expression
-        } else if (e.target.innerHTML === 'AC') {
-            expression = ''
-            input.value = expression
-        } else if (e.target.innerHTML === 'DEL') {
-            expression = expression.slice(0, expression.length-1)
-            input.value = expression
-        } else {
-            expression += e.target.innerHTML
-            input.value = expression
-        }
+        if (e.target.innerHTML === '=') expression = eval(expression);
+        else if (e.target.innerHTML === 'AC') expression = '';
+        else if (e.target.innerHTML === 'DEL') expression = expression.slice(0, expression.length - 1);
+        else expression += e.target.innerHTML;
+
+        input.value = expression;
     })
+})
+
+window.addEventListener("keydown", e => {
+    if (e.key == 'Enter' || e.key == "=") expression = eval(expression)
+    else if (e.key == 'Backspace') expression = expression.slice(0, expression.length - 1);
+    else if (arr.some(btn => btn.innerText === e.key)) expression += e.key;
+    input.value = expression
 })
